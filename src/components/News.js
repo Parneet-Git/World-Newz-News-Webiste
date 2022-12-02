0d374060de4f4edb0d95a4267067d45378422ec5
Parneet Singh Sarao <parneet1650.be20@chitkara.edu.in>
2022-12-02 17:56:02 +0530

    fixed api fetching data

--- src/components/News.js
+++ src/components/News.js
@@ -19,11 +19,37 @@ const News = (props) => {
 
   const updateNews = async () => {
     props.setProgress(10);
-    console.log(props.apiKey);
     let options = {
       method: 'GET',
-      url: 'https://api.newscatcherapi.com/v2/sources',
-      params: { topic: props.category, lang: 'en', sort_by: 'relevancy', page: props.pageSize },
+      url: 'https://api.newscatcherapi.com/v2/search',
+      params: { q: props.category, lang: 'en', sort_by: 'relevancy', page_size: props.pageSize },
+      headers: {
+        'x-api-key': props.apiKey
+      }
+    };
+    props.setProgress(30);
+    setLoading(true);
+    let parsedData;
+    await axios.request(options).then(function (response) {
+      parsedData = response.data;
+    }).catch(function (error) {
+      console.error(error);
+    });
+    props.setProgress(70);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.total_hits);
+    setLoading(false);
+    props.setProgress(100);
+    window.scrollTo(0, 0);
+  }
+
+  const searchQuery = async (query) => {
+    props.setProgress(10);
+    await setPage(1);
+    let options = {
+      method: 'GET',
+      url: 'https://api.newscatcherapi.com/v2/search',
+      params: { q: query, lang: 'en', sort_by: 'relevancy', page_size: props.pageSize },
       headers: {
         'x-api-key': props.apiKey
       }
@@ -38,32 +64,15 @@ const News = (props) => {
       console.error(error);
     });
     props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalhits);
-    setLoading(false);
-    props.setProgress(100);
-    window.scrollTo(0, 0);
-  }
-
-  const searchQuery = async (query) => {
-    props.setProgress(10);
-    await setPage(1);
-    let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
-    props.setProgress(30);
-    setLoading(true);
-    let data = await fetch(url);
-    props.setProgress(50);
-    let parsedData = await data.json();
     props.setProgress(70);
     setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
+    setTotalResults(parsedData.total_hits);
     setLoading(false);
 
     props.setProgress(100);
   }
 
   // this function runs after the render function
-
   useEffect(() => {
     updateNews();
     let searchBtn = document.querySelector('.search-icon');
@@ -109,8 +118,8 @@ const News = (props) => {
       > */}
         <div className="container">
           <div className="news-cards-container">
-            {articles.map((elem) => {
-              return <div key={elem.summary}>
+            {articles.map((elem, idx) => {
+              return <div key={idx}>
                 <NewsItem title={elem.title ? elem.title : ""} description={elem.summary ? elem.summary : ""} author={elem.author} imageUrl={elem.media ? elem.media : "../images/no_news_img.jpg"} newsUrl={elem.link ? elem.link : ""} source={elem.rights} />
               </div>
             })}
